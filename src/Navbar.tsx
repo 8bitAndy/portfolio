383012413431cd4847a0ec367b2a44f1faeb05e5
Liam <86295301+8bitAndy@users.noreply.github.com>
2023-02-14 22:02:10 +0800

    Added mobile navigation menu and basic mobile view

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,8 +1,14 @@
-
-
-
 // Navbar component for website
 export default function Navbar() {
+    // State of the hamburger menu and mobile menu
+    const burgerButton = document.querySelector('.Hamburger');
+    const mobileMenu = document.querySelector('.MobileNav')
+
+    // Deals with the opening and closing of hamburger menu, also the only time I can possibly use name a name with burger in it
+    const handleBurger = () => {
+        burgerButton?.classList.toggle('is-active');
+        mobileMenu?.classList.toggle('is-active');
+    }
 
     window.onscroll = () => {
         scrollFunction()
@@ -12,19 +18,33 @@ export default function Navbar() {
     function scrollFunction() {
         if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
             document.getElementById("navbar")?.classList.add('change');
+            document.getElementById("burgerBar")?.classList.add('is-scroll');
         } else {
-            document.getElementById("navbar")?.classList.remove('change');
+            document.getElementById("navbar")?.classList.remove('change')
+            document.getElementById("burgerBar")?.classList.remove('is-scroll')
         }
     }
 
     return (
         <div className="Parent">
             <div id="navbar" className="Navbar">
-                <ul>
+                <ul className="DesktopNav">
                     <li><a href="#top">Top</a></li>
                     <li><a href="#about-me">About me</a></li>
                     <li><a href="#my-portfolio">My Portfolio</a></li>
                     <li><a href="#contact">Contact</a></li>
+                </ul>
+                <button className="Hamburger" onClick={handleBurger}>
+                    <div className="Bar" id="burgerBar">
+                    </div>
+                </button>
+            </div>
+            <div className="MobileNav">
+                <ul>
+                    <li><a href="#top" onClick={handleBurger}>Top</a></li>
+                    <li><a href="#about-me" onClick={handleBurger}>About me</a></li>
+                    <li><a href="#my-portfolio" onClick={handleBurger}>My Portfolio</a></li>
+                    <li><a href="#contact" onClick={handleBurger}>Contact</a></li>
                 </ul>
             </div>
         </div>
