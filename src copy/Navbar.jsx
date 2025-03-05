1e093b0cd34fafb4b0dbe36e16020a791088789d
Liam <86295301+8bitAndy@users.noreply.github.com>
2025-03-05 22:21:27 +1100

    first commit

---
+++ src copy/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return<>
+        <ul className="main-menu">
+            <li>Home</li>
+            <li>My Work</li>
+            <li>My Resume</li>
+            <li>Get in contact</li>
+        </ul>
+    </>
+}
---
+++ src copy/Placeholder.jsx
@@ -0,0 +1,7 @@
+import Introduction from "./Introduction"
+
+export default function Placeholder() {
+    return <div className="box">
+        <Introduction/>
+    </div>
+}
