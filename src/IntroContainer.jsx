8014fe40bda7f8a53464a9b7c3acc325490c8a76
Liam <contact@liamandrews.dev>
2025-10-09 22:16:11 +1100

    Added basic styling and formatting to main column

--- src/IntroContainer.jsx
+++ src/IntroContainer.jsx
@@ -5,12 +5,17 @@ export default function IntroContainer() {
     return (
         <div>
             <div className='header'>
-                <img src={headshot} className='headshot'/>
+                <img src={headshot} className='headshot' />
                 <div className='name-title'>
                     <h1>Liam Andrews</h1>
                     <h4>Junior Software Engineer</h4>
                 </div>
-                
+            </div>
+            <p>I am a passionate software developer that loves making cool looking user interface. My experiences</p>
+            <div className='button-container'>
+                <button className='button-download'>Download CV</button>
+                <button className='button-social-link'>&lt;--&gt;</button>
+                <button className='button-social-link'>&lt;--&gt;</button>
             </div>
         </div>
     )
